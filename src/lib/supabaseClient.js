fff971930bf74ba13f5d621e0ed9b45d24790ddb
Nigel Petargue <nigelpetargue@gmail.com>
2025-01-26 22:49:02 +0800

    make url public

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://lpfgvawehdwnurkcovrg.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxwZmd2YXdlaGR3bnVya2NvdnJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc4OTU2OTQsImV4cCI6MjA1MzQ3MTY5NH0.QJEuZCtEkkImk9_8i5G26Xp7YssL7AOeb2kXocEztIM'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
